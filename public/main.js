cbe82618fbb919430fcabfdf015d247e57d4528a
Vuka951 <vultashin76@gmail.com>
2019-03-24 15:35:10 +0100

    main js config update and electron-devtools

--- public/main.js
+++ public/main.js
@@ -5,7 +5,21 @@ const isDev = require('electron-is-dev');
 
 let mainWindow;
 
+const installExtensions = () => {
+  const {default: installExtension, REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS} = require('electron-devtools-installer');
+
+  installExtension(REACT_DEVELOPER_TOOLS)
+    .then((name) => console.log(`Added Extension:  ${name}`))
+    .catch((err) => console.log('An error occurred: ', err));
+  installExtension(REDUX_DEVTOOLS)
+    .then((name) => console.log(`Added Extension:  ${name}`))
+    .catch((err) => console.log('An error occurred: ', err));
+};
+
 function createWindow() {
+  if (isDev) {
+    installExtensions();
+  }
   mainWindow = new BrowserWindow({width: 900, height: 680});
   mainWindow.loadURL(isDev ? 'http://localhost:8080' : url.format({
     pathname: path.join(__dirname, '../web-build/index.html'),
@@ -18,7 +32,9 @@ function createWindow() {
   isDev ? server = null : server = require(path.join(__dirname, '../server-build/bundle-back.js'));
 
   mainWindow.on('closed', () => mainWindow = null);
-  mainWindow.webContents.openDevTools();
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
 }
 
 app.on('ready', createWindow);
